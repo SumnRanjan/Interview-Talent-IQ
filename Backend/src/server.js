c43b3f22a70d471671a121757adefa8671602fe5
Suman Ranjan <sumanranjan17291729@gmail.com>
2025-12-17 12:40:47 +0530

    deployment config added

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,24 @@
 import express from "express"
+import path from 'path'
 import { ENV } from "./lib/env.js"
 
 const app = express()
 
-app.get("/" , (req , res)=>{
+
+const __dirname = path.resolve()
+
+app.get("/hello" , (req , res)=>{
     res.status(200).json({msg : "success form apiss"})
 })
+
+//make ready for deploy
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname , "../FrontEnd/dist")))
+
+    app.get("/{*any}" , (req , res)=>{
+        res.sendFile(path.join(__dirname , "../FrontEnd" , "dist" , "index.html"))
+    })
+}
 
 app.listen(ENV.PORT , ()=>{
     console.log("Server is running at port 3000")
